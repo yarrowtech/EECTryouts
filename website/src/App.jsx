6e3de96626a0b1558bda1af43f47dd48c65d6be0
Azzpect <atanughosh.brp@gmail.com>
2025-07-02 18:43:48 +0530

    completed drag drop game

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,8 +1,13 @@
+import { Route, Routes } from "react-router-dom"
+import DragDrop from "./games/drag_drop"
 
 function App() {
 
   return (
-    <h1 className="text-yellow-400 text-xl font-bold">First Commit</h1>
+    <Routes>
+      <Route path="/" element={<h1>Welcome to the Game Portal</h1>} />
+      <Route path="/drag-drop" element={<DragDrop />} />
+    </Routes>
   )
 }
 
