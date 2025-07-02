0d47c20c408c89c24c3c89d8e4763654b53dd0ff
Azzpect <atanughosh.brp@gmail.com>
2025-07-02 18:47:58 +0530

    fixed font weight and added mcq route

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import DragDrop from "./games/drag_drop"
+import MCQ from "./games/mcq"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<h1>Welcome to the Game Portal</h1>} />
       <Route path="/drag-drop" element={<DragDrop />} />
+      <Route path="/mcq" element={<MCQ />} />
     </Routes>
   )
 }
